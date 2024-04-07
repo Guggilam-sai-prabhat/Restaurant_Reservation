55282275208fb46a0bd8db5b54bc836ce6cd5466
Prabhat0321 <guggliamprabhat@gmail.com>
2024-04-07 21:24:54 +0530

    app.js updated

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1/reservation",reservationRouter);
 
-app.get("/", (req, res, next)=>{return res.status(200).json({
+app.get("/", (req, res, next)=>{return res.status(200)
+  .json({
     success: true,
     message: "HELLO WORLD AGAIN"
   })})
