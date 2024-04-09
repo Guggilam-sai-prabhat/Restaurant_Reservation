07b0635871a22680813ff7e305c77b6456d77a38
Prabhat0321 <guggliamprabhat@gmail.com>
2024-04-09 08:05:10 +0530

    URL CHANGED

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config({path : "./config.env"});
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://restaurant-reservation-dun.vercel.app/"],
     credentials: true,
     methods: [ 'POST']
 
