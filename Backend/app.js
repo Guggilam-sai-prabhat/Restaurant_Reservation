b856f3561e48cc965c82014366776b6a28a71506
Prabhat0321 <guggliamprabhat@gmail.com>
2024-04-09 08:19:29 +0530

    Updated

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config({path : "./config.env"});
 
 app.use(cors({
-    origin: ["https://restaurant-reservation-dun.vercel.app/"],
+    origin: ["https://restaurant-reservation-dun.vercel.app"],
     credentials: true,
     methods: [ 'POST']
 
